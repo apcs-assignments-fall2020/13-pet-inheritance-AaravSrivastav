cea40c5aefd93d1f4d0d481f15016c7990bffe87
AaravSrivastav <22asrivastav@athenian.org>
2020-11-01 23:17:44 +0000

    added the dog class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,46 @@
 public class Dog extends Pet {
     // Instance variable(s)
 
+    private String breed;
 
     // Constructors
     
+    public Dog(String name, int age, String breed)
+    {
+        super(name, age);
+        this.breed = breed;
 
+    }
+    
+    public Dog()
+    {
+        super("max", 1);
+    }
     // makeNoise() method
 
+    public void makeNoise()
+    {
+        System.out.println("woof");
+    }
 
     // toString method
-
+    public String toString() 
+    {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() 
+    {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) 
+    {
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
 }
